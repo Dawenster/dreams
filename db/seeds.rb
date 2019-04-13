aa495f1219f0d610c8cf271e7526655333b2cb40
David Wen <Dawenster@users.noreply.github.com>
2019-04-12 21:46:32 -0700

    Create seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1000.times do
+  user = User.create!(email: Faker::Internet.email)
+  animal = Faker::Creature::Animal.name
+  color = Faker::Color.color_name
+  verb = Faker::Verb.simple_present
+  objective = ['towards', 'from', 'to', 'at', 'into', 'under', 'beside'].sample
+  word1, word2 = Faker::Hipster.words(2, false, true)
+
+  title = "#{color.capitalize} #{word1} #{animal} #{verb} #{objective} a #{word2}"
+
+  Dream.create(
+    user: user,
+    title: title
+  )
+
+  puts "Created dream with title: #{title}"
+end
