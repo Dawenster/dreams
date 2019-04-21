f51a6646466ad0df7e23e52adaa33ebc474e7637
David Wen <Dawenster@users.noreply.github.com>
2019-04-21 11:34:53 -0700

    Update charge annotation

--- spec/models/charge_spec.rb
+++ spec/models/charge_spec.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: charges
+#
+#  id                :uuid             not null, primary key
+#  amount_in_cents   :integer          not null
+#  currency          :string           not null
+#  fee_in_cents      :integer          not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  payment_method_id :uuid             not null
+#  stripe_charge_id  :string           not null
+#  user_id           :uuid             not null
+#
+# Indexes
+#
+#  index_charges_on_payment_method_id  (payment_method_id)
+#  index_charges_on_user_id            (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (payment_method_id => payment_methods.id)
+#  fk_rails_...  (user_id => users.id)
+#
+
 require 'rails_helper'
 
 RSpec.describe Charge, type: :model do
